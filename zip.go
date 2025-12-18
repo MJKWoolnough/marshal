f131cff5582d6718fee2bdc4c3d88c7e675fb682
Michael Woolnough <michael.woolnough@gmail.com>
2025-12-18 21:21:26 +0000

    added implementation of Stat for zipfs

--- zip.go
+++ zip.go
@@ -3,6 +3,9 @@ package main
 import (
 	"archive/zip"
 	"io/fs"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 type zipFS struct {
@@ -20,4 +23,31 @@ func (z *zipFS) ReadDir(name string) ([]fs.DirEntry, error) {
 	}
 
 	return nil, fs.ErrInvalid
+}
+
+type zipDir string
+
+func (z zipDir) Name() string     { return filepath.Base(string(z)) }
+func (zipDir) Size() int64        { return 0 }
+func (zipDir) Mode() fs.FileMode  { return fs.ModeDir | fs.ModePerm }
+func (zipDir) ModTime() time.Time { return time.Now() }
+func (zipDir) IsDir() bool        { return true }
+func (z zipDir) Sys() any         { return z }
+
+func (z *zipFS) Stat(path string) (fs.FileInfo, error) {
+	pathWithSlash := path
+
+	if !strings.HasSuffix(path, "/") {
+		pathWithSlash += "/"
+	}
+
+	for _, f := range z.File {
+		if f.Name == path {
+			return f.FileInfo(), nil
+		} else if strings.HasPrefix(f.Name, pathWithSlash) {
+			return zipDir(path), nil
+		}
+	}
+
+	return nil, fs.ErrNotExist
 }
